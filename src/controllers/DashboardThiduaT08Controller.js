09da00d0727bfac94435588daabb44b8ac1c9c36
MobiFone7 <duong.tranvan@mobifone.vn>
2025-08-19 15:09:42 +0700

    up

--- src/controllers/DashboardThiduaT08Controller.js
+++ src/controllers/DashboardThiduaT08Controller.js
@@ -105,6 +105,102 @@ class DashboardThiduaT08Controller {
         }
     }
 
+    async createManualListLTTSL(req, res) {
+
+        const result = validationResult(req);
+        if (result.isEmpty()) {
+            const kpiList = req.body.kpiList;
+            if (kpiList && kpiList.length > 0) {
+                try {
+                    console.log("kpiList", kpiList);
+                    for (const object of kpiList) {
+                        let info = {
+                            amount: object.amount ?? 0,
+                            date: object.date,
+                            province: object.province
+
+                        }
+
+                        const existingKpi = await sequelize.query(
+                            `SELECT * from sl_thidua_ltt_082025 WHERE issue_date =  to_date(:date,'dd-mm-rrrr')
+                                    and province = :province
+                                    `,
+                            {
+                                replacements: { date: info.date, province: info.province },
+                                type: sequelize.QueryTypes.SELECT,
+                            }
+                        );
+
+                        console.log("existingKpi", existingKpi)
+                        if (existingKpi.length > 0) {
+                            await sequelize.query(
+                                `delete from sl_thidua_ltt_082025 
+                                            WHERE  issue_date = to_date(:date,'dd-mm-rrrr')
+                                        and province = :province
+                                            `,
+                                {
+                                    replacements: {
+                                        date: info.date,
+                                        province: info.province,
+                                    },
+                                    type: sequelize.QueryTypes.DELETE,
+                                }
+                            );
+                            await sequelize.query(
+                                `insert into  sl_thidua_ltt_082025 
+                                    (issue_date, province, amount)
+                                    values
+                                    (
+                                    to_date(:date,'dd-mm-rrrr'),
+                                    :province,
+                                    :amount
+                                        )
+                                    `,
+                                {
+                                    replacements: {
+                                        date: info.date,
+                                        province: info.province,
+                                        amount: info.amount
+                                    },
+                                    type: sequelize.QueryTypes.INSERT,
+                                }
+                            );
+
+                            // console.log("check ne", result);
+
+                        } else {
+
+                            await sequelize.query(
+                                `insert into  sl_thidua_ltt_082025 
+                                        (issue_date, province, amount)
+                                        values
+                                        (
+                                        to_date(:date,'dd-mm-rrrr'),
+                                        :province,
+                                        :amount
+                                            )
+                                        `,
+                                {
+                                    replacements: {
+                                        date: info.date,
+                                        province: info.province,
+                                        amount: info.amount
+                                    },
+                                    type: sequelize.QueryTypes.INSERT,
+                                }
+                            );
+
+                        };
+                    }
+                    res.send({ data: { sussess: true } })
+                } catch (error) {
+                    throw new Error(`Có lỗi xảy ra:  ${error}`)
+
+                }
+            }
+        }
+    }
+
     async createManualListCamera(req, res) {
 
         const result = validationResult(req);
@@ -320,6 +416,30 @@ class DashboardThiduaT08Controller {
 
     }
 
+    getSoLuongLTTDetail(req, res) {
+        const date = req.query.date;
+        if (date) {
+            const query = `select province, issue_date, amount   from sl_thidua_ltt_082025
+            where issue_date =  to_date(:date,'dd/mm/rrrr')
+            order by  issue_date, province  `;
+            sequelize.query(query, {
+                replacements: { date: date },
+                type: sequelize.QueryTypes.SELECT
+            })
+                .then(data => {
+                    res.json({ data });
+                })
+                .catch(err => {
+                    console.error("Error fetching Thidua  data:", err);
+                    res.status(500).json({ error: "Internal Server Error" });
+                });
+        } else {
+            res.status(500).json({ error: "Thiếu thông tin ngày" });
+        }
+
+    }
+
+
     getDoanhthuCameraDetail(req, res) {
         const date = req.query.date;
         if (date) {
@@ -384,6 +504,25 @@ class DashboardThiduaT08Controller {
                 res.status(500).json({ error: "Internal Server Error" });
             });
     }
+
+    getSoLuongLTT(req, res) {
+        const query = `select province, issue_date, amount   from sl_thidua_ltt_082025
+            where issue_date >=  to_date('01/08/2025','dd/mm/rrrr')
+            and issue_date <  to_date('01/09/2025','dd/mm/rrrr')
+            order by  issue_date, province`;
+        sequelize.query(query, {
+            replacements: {},
+            type: sequelize.QueryTypes.SELECT
+        })
+            .then(data => {
+                res.json({ data });
+            })
+            .catch(err => {
+                console.error("Error fetching Thidua  data:", err);
+                res.status(500).json({ error: "Internal Server Error" });
+            });
+    }
+
 
     getDoanhthuCamera(req, res) {
         const query = `select province, issue_date, amount   from thidua_camera_082025
--- src/routes/DashBoardThiduaT08.js
+++ src/routes/DashBoardThiduaT08.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const controller = require("../controllers/DashboardThiduaT08Controller");
 
 router.get("/dthu-thidua-ltt", controller.getDoanhthuLTT);
+router.get("/sl-thidua-ltt", controller.getSoLuongLTT);
 router.get("/dthu-thidua-camera", controller.getDoanhthuCamera);
 router.get("/dthu-thidua-camera-sl", controller.getSoLuongCamera);
 router.get("/dthu-thidua-camera-detail", controller.getDoanhthuCameraDetail);
 router.get("/sl-thidua-camera-detail", controller.getSoLuongCameraDetail);
 router.get("/dthu-thidua-ltt-detail", controller.getDoanhthuLTTDetail);
+router.get("/sl-thidua-ltt-detail", controller.getSoLuongLTTDetail);
 router.post("/dthu-thidua-camera", controller.createManualListCamera);
 router.post("/sl-thidua-camera", controller.createManualListCameraSL);
 router.post("/dthu-thidua-ltt", controller.createManualListLTT);
+router.post("/sl-thidua-ltt", controller.createManualListLTTSL);
 router.use("/", controller.index);
 
 module.exports = router;
