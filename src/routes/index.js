32b172e81f95e5828e651c4f3dcdb89325d47814
MobiFone7 <duong.tranvan@mobifone.vn>
2025-12-24 14:28:23 +0700

    up

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,14 +21,25 @@ function route(app) {
 
 }
 function authenticateToken(req, res, next) {
-  // const authHeader = req.headers["authorization"];
-  // const token = authHeader && authHeader.split(" ")[1];
-  // if (token == null) return res.sendStatus(401);
-  // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-  //   if (err) return res.sendStatus(403);
-  //   req.user = user;
-  //   next();
-  // });
-  next();
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Missing Authorization header" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Missing token" });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+
+    req.user = user;
+    next(); // ✅ CHỈ GỌI 1 LẦN
+  });
 }
 module.exports = route;
