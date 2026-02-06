586220aaa238930d068061a62e4e5b831d780482
MobiFone7 <duong.tranvan@mobifone.vn>
2026-02-06 09:10:48 +0700

    up

--- src/controllers/ReportController.js
+++ src/controllers/ReportController.js
@@ -23,67 +23,75 @@ class ReportController {
     if (monthString && startOfMonth) {
       let sql;
       sql = `
+            WITH base_kpi AS (
+                SELECT
+                    a.thang,
+                    a.goi_cuoc,
+                    b.area_code,
+                    SUM(NVL(a.trong_tinh,0))  AS trong_tinh,
+                    SUM(NVL(a.ngoai_tinh,0)) AS ngoai_tinh
+                FROM db01_owner.th_tb_ptsl_th_bc03_show a
+                LEFT JOIN (
+                    SELECT ward_code, MIN(area_code) area_code
+                    FROM db01_owner.map_area_ward_new
+                    GROUP BY ward_code
+                ) b
+                  ON a.precinct_code = b.ward_code
+                WHERE a.thang = DATE '2026-02-01'
+                GROUP BY
+                    a.thang,
+                    a.goi_cuoc,
+                    b.area_code
+            ),
+
+            total_kpi AS (
+                SELECT
+                    thang,
+                    goi_cuoc,
+                    SUM(trong_tinh)  AS tong_trong_tinh,
+                    SUM(ngoai_tinh) AS tong_ngoai_tinh
+                FROM base_kpi
+                GROUP BY thang, goi_cuoc
+            )
+
             SELECT
-                t.*,
-
-                 NVL(DLA_T01_TRONG_TINH,0) + NVL(DLA_T02_TRONG_TINH,0) + NVL(DLA_T03_TRONG_TINH,0) +
-                NVL(DLA_T04_TRONG_TINH,0) + NVL(DLA_T05_TRONG_TINH,0) + NVL(DLA_T06_TRONG_TINH,0) +
-                NVL(DLA_T07_TRONG_TINH,0) + NVL(DLA_T08_TRONG_TINH,0) + NVL(DLA_T09_TRONG_TINH,0) +
-                NVL(DLA_T10_TRONG_TINH,0) + NVL(DLA_T11_TRONG_TINH,0) + NVL(DLA_T12_TRONG_TINH,0) +
-                NVL(DLA_T13_TRONG_TINH,0) + NVL(DLA_D01_TRONG_TINH,0) +  NVL(DLA_D02_TRONG_TINH,0) +
-                 NVL(DLA_D03_TRONG_TINH,0) +  NVL(DLA_D04_TRONG_TINH,0) +  NVL(DLA_D05_TRONG_TINH,0)+
-                  NVL(DLA_D06_TRONG_TINH,0) AS tong_trong_tinh,
-
-                NVL(DLA_T01_NGOAI_TINH,0) + NVL(DLA_T02_NGOAI_TINH,0) + NVL(DLA_T03_NGOAI_TINH,0) +
-                NVL(DLA_T04_NGOAI_TINH,0) + NVL(DLA_T05_NGOAI_TINH,0) + NVL(DLA_T06_NGOAI_TINH,0) +
-                NVL(DLA_T07_NGOAI_TINH,0) + NVL(DLA_T08_NGOAI_TINH,0) + NVL(DLA_T09_NGOAI_TINH,0) +
-                NVL(DLA_T10_NGOAI_TINH,0) + NVL(DLA_T11_NGOAI_TINH,0) + NVL(DLA_T12_NGOAI_TINH,0) +
-                NVL(DLA_T13_NGOAI_TINH,0) + NVL(DLA_D01_NGOAI_TINH,0) + NVL(DLA_D02_NGOAI_TINH,0) + NVL(DLA_D03_NGOAI_TINH,0) +
-                NVL(DLA_D04_NGOAI_TINH,0) + NVL(DLA_D05_NGOAI_TINH,0) + NVL(DLA_D06_NGOAI_TINH,0) 
-                AS tong_ngoai_tinh
-
-            FROM (
-                SELECT *
-                FROM (
-                    SELECT
-                        a.thang,
-                        a.goi_cuoc,
-                        b.area_code,
-                        NVL(a.trong_tinh,0)  AS sl_tb,
-                        NVL(a.ngoai_tinh,0) AS sl_tb_1
-                    FROM db01_owner.th_tb_ptsl_th_bc03_show a
-                    LEFT JOIN db01_owner.map_area_ward_new b
-                        ON a.precinct_code = b.ward_code
+                p.*,
+                t.tong_trong_tinh,
+                t.tong_ngoai_tinh
+            FROM base_kpi
+            PIVOT (
+                SUM(trong_tinh)  AS trong_tinh,
+                SUM(ngoai_tinh) AS ngoai_tinh
+                FOR area_code IN (
+                    'DLA_T01' AS DLA_T01,
+                    'DLA_T02' AS DLA_T02,
+                    'DLA_T03' AS DLA_T03,
+                    'DLA_T04' AS DLA_T04,
+                    'DLA_T05' AS DLA_T05,
+                    'DLA_T06' AS DLA_T06,
+                    'DLA_T07' AS DLA_T07,
+                    'DLA_T08' AS DLA_T08,
+                    'DLA_T09' AS DLA_T09,
+                    'DLA_T10' AS DLA_T10,
+                    'DLA_T11' AS DLA_T11,
+                    'DLA_T12' AS DLA_T12,
+                    'DLA_T13' AS DLA_T13,
+                    'DLA_D01' AS DLA_D01,
+                    'DLA_D02' AS DLA_D02,
+                    'DLA_D03' AS DLA_D03,
+                    'DLA_D04' AS DLA_D04,
+                    'DLA_D05' AS DLA_D05,
+                    'DLA_D06' AS DLA_D06
                 )
-                PIVOT (
-                    SUM(sl_tb)   AS trong_tinh,
-                    SUM(sl_tb_1) AS ngoai_tinh
-                    FOR area_code IN (
-                        'DLA_T01' AS DLA_T01,
-                        'DLA_T02' AS DLA_T02,
-                        'DLA_T03' AS DLA_T03,
-                        'DLA_T04' AS DLA_T04,
-                        'DLA_T05' AS DLA_T05,
-                        'DLA_T06' AS DLA_T06,
-                        'DLA_T07' AS DLA_T07,
-                        'DLA_T08' AS DLA_T08,
-                        'DLA_T09' AS DLA_T09,
-                        'DLA_T10' AS DLA_T10,
-                        'DLA_T11' AS DLA_T11,
-                        'DLA_T12' AS DLA_T12,
-                        'DLA_T13' AS DLA_T13,
-                        'DLA_D01' AS DLA_D01,
-                        'DLA_D02' AS DLA_D02,
-                        'DLA_D03' AS DLA_D03,
-                        'DLA_D04' AS DLA_D04,
-                        'DLA_D05' AS DLA_D05,
-                        'DLA_D06' AS DLA_D06
-                    )
-                )
-            ) t   
-            where thang = TO_DATE('${startOfMonth}','dd/mm/rrrr')
-            ORDER BY thang, goi_cuoc
+            ) p
+            JOIN total_kpi t
+              ON p.thang = t.thang
+            AND p.goi_cuoc = t.goi_cuoc
+            where p.thang = TO_DATE('${startOfMonth}','dd/mm/rrrr')
+            ORDER BY p.thang, p.goi_cuoc
           `;
+
+
 
       DbConnection.getConnected(sql, {}, function (result) {
         if (result) {
